3b105307ba525784b53c8c7bd611d0e6e64dae35
Bùi Vũ Anh <43056421+vuanhlk12@users.noreply.github.com>
2022-02-27 21:41:47 +0700

    s

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ import authRoute from "./routes/auth";
 import postRoute from "./routes/post";
 import conversationRoute from "./routes/conversations";
 import messageRoute from "./routes/messages";
+import { Server } from "socket.io";
+import * as http from "http";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 dotenv.config();
 
 mongoose.connect(
@@ -39,7 +44,7 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-app.post("/api/upload", upload.single("file"), (req, res) => {
+app.post("/upload", upload.single("file"), (req, res) => {
   try {
     return res.status(200).json("File uploded successfully");
   } catch (error) {
@@ -52,6 +57,48 @@ app.use("/auth", authRoute);
 app.use("/posts", postRoute);
 app.use("/conversations", conversationRoute);
 app.use("/messages", messageRoute);
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  //when ceonnect
+  console.log("a user connected.");
+
+  //take userId and socketId from user
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    io.to(user.socketId).emit("getMessage", {
+      senderId,
+      text,
+    });
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
 
 app.listen(8800, () => {
   console.log("BE running", 8800);
