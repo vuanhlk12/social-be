5d1d1f78a60a78613922e9bbccdd4adf74b8a7ab
Bùi Vũ Anh <43056421+vuanhlk12@users.noreply.github.com>
2022-03-02 16:11:54 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,24 +5,23 @@ const verify = require("../verifyToken");
 
 //update user
 router.put("/:id", verify, async (req, res) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
-    if (req.body.password) {
-      req.body.password = CryptoJS.AES.encrypt(
-        req.body.password,
-        process.env.SECRET_KEY
-      ).toString();
-    }
-
-    try {
+  try {
+    if (req?.user?.id === req?.params?.id || req?.user?.isAdmin) {
+      if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+          req.body.password,
+          process.env.SECRET_KEY
+        ).toString();
+      }
       const updatedUser = await User.findByIdAndUpdate(req.params.id, {
         $set: req.body,
       });
       res.status(200).json(updatedUser);
-    } catch (err) {
-      res.status(500).json(err);
+    } else {
+      res.status(403).json("You can update only your account!");
     }
-  } else {
-    res.status(403).json("You can update only your account!");
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
